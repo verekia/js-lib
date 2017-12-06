5a48408e9f070884e1c73d23c2a09d7310a08972
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-12-07 00:22:18 +0100

    Restructured project for easy imports

---
+++ consumer.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var _swit = require('./swit');
+
+var _swit2 = _interopRequireDefault(_swit);
+
+var _cond = require('./cond');
+
+var _cond2 = _interopRequireDefault(_cond);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* eslint-disable no-console */
+
+console.log(_swit2.default);
+console.log(_cond2.default);
